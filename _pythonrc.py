67007e6c3ef0f2bf79bafcd6fbc9af305fa6e0f7
Emlyn Corrin <emlyn@corrin.name>
2011-04-05 16:36:32 +0200

    Updated pythonrc.py

--- _pythonrc.py
+++ _pythonrc.py
@@ -1,136 +1,44 @@
-"""
-This file is executed when the Python interactive shell is started if
-$PYTHONSTARTUP is in your environment and points to this file. It's just
-regular Python commands, so do what you will. Your ~/.inputrc file can greatly
-complement this file.
+#from __future__ import print_function
+#try:
+#    from utils import *
+#except ImportError:
+#    print "Module utils not available."
 
-"""
-import os
+import sys, os
+sys.path.append(os.environ["HOME"] + "/lib")
+del sys, os
+
+try:
+    import emlyn
+    for sub in emlyn.__all__:
+        m = __import__("emlyn." + sub, fromlist=["*"])
+        for n in dir(m):
+            if not n[0].isalpha(): continue
+            globals()[n] = m.__getattribute__(n)
+except ImportError:
+    print("Emlyn library not available")
 
 try:
     import readline
 except ImportError:
     print("Module readline not available.")
 else:
+    class _History:
+        def __init__(self):
+            self.rl = readline
+        def __getitem__(self, i):
+            l = len(self)
+            if i < 0: i = l+i
+            if i >= l or i < 0: raise IndexError("history index out of range")
+            return self.rl.get_history_item(i)
+        def __len__(self):
+            return self.rl.get_current_history_length()
+        def __str__(self):
+            return str([self[i] for i in xrange(len(self))])
+        def __repr__(self):
+            return str(self)
+    hist = _History()
     import rlcompleter
     readline.parse_and_bind("tab: complete")
-
-    # Enable a History
-    HISTFILE="%s/.pyhistory" % os.environ["HOME"]
-
-    # Read the existing history if there is one
-    if os.path.exists(HISTFILE):
-        readline.read_history_file(HISTFILE)
-
-    # Set maximum number of items that will be written to the history file
-    readline.set_history_length(300)
-
-    def savehist():
-        readline.write_history_file(HISTFILE)
-
-    import atexit
-    atexit.register(savehist)
-finally:
-    del rlcompleter
-    del atexit
-
-WELCOME=''
-# Color Support
-class TermColors(dict):
-    """Gives easy access to ANSI color codes. Attempts to fall back to no color
-    for certain TERM values. (Mostly stolen from IPython.)"""
-
-    COLOR_TEMPLATES = (
-        ("Black"       , "0;30"),
-        ("Red"         , "0;31"),
-        ("Green"       , "0;32"),
-        ("Brown"       , "0;33"),
-        ("Blue"        , "0;34"),
-        ("Purple"      , "0;35"),
-        ("Cyan"        , "0;36"),
-        ("LightGray"   , "0;37"),
-        ("DarkGray"    , "1;30"),
-        ("LightRed"    , "1;31"),
-        ("LightGreen"  , "1;32"),
-        ("Yellow"      , "1;33"),
-        ("LightBlue"   , "1;34"),
-        ("LightPurple" , "1;35"),
-        ("LightCyan"   , "1;36"),
-        ("White"       , "1;37"),
-        ("Normal"      , "0"),
-    )
-
-    NoColor = ''
-    _base  = '\001\033[%sm\002'
-
-    def __init__(self):
-        if os.environ.get('TERM') in ('xterm-color', 'xterm-256color', 'linux',
-                                    'screen', 'screen-256color', 'screen-bce'):
-            self.update(dict([(k, self._base % v) for k,v in self.COLOR_TEMPLATES]))
-        else:
-            self.update(dict([(k, self.NoColor) for k,v in self.COLOR_TEMPLATES]))
-_c = TermColors()
-
-
-
-import sys
-# Enable Color Prompts
-sys.ps1 = '%s>>> %s' % (_c['Green'], _c['Normal'])
-sys.ps2 = '%s... %s' % (_c['Red'], _c['Normal'])
-
-# Enable Pretty Printing for stdout
-def my_displayhook(value):
-    if value is not None:
-        try:
-            import __builtin__
-            __builtin__._ = value
-        except ImportError:
-            __builtins__._ = value
-
-        import pprint
-        pprint.pprint(value)
-        del pprint
-
-sys.displayhook = my_displayhook
-
-# clean up namespace
-del sys
-
-# Start an external editor with \e
-# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/438813/
-
-from tempfile import mkstemp
-from code import InteractiveConsole
-
-EDITOR = os.environ.get('EDITOR', 'vim')
-EDIT_CMD = '\e'
-
-class EditableBufferInteractiveConsole(InteractiveConsole):
-    def __init__(self, *args, **kwargs):
-        self.last_buffer = [] # This holds the last executed statement
-        InteractiveConsole.__init__(self, *args, **kwargs)
-
-    def runsource(self, source, *args):
-        self.last_buffer = [ source.encode('utf-8') ]
-        return InteractiveConsole.runsource(self, source, *args)
-
-    def raw_input(self, *args):
-        line = InteractiveConsole.raw_input(self, *args)
-        if line == EDIT_CMD:
-            fd, tmpfl = mkstemp('.py')
-            os.write(fd, b'\n'.join(self.last_buffer))
-            os.close(fd)
-            os.system('%s %s' % (EDITOR, tmpfl))
-            lines = open(tmpfl).read().split('\n')
-            os.unlink(tmpfl)
-            tmpfl = ''
-            for i in xrange(len(lines) - 1): self.push(lines[i])
-            line = lines[-1]
-        return line
-
-c = EditableBufferInteractiveConsole(locals=locals())
-c.interact(banner=WELCOME)
-
-# Exit the Python shell on exiting the InteractiveConsole
-import sys
-sys.exit()
+    #readline.parse_and_bind("bind ^I rl_complete") # for Leopard
+    del readline, rlcompleter, _History
